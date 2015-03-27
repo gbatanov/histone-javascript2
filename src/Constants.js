b275cd9687a89d53651d7443670c9fa60f251e5d
Ruslan <matveev.ruslan@gmail.com>
2015-03-28 02:23:00 +0300

    cleanup

--- src/Constants.js
+++ src/Constants.js
@@ -1,14 +1,13 @@
 module.exports = {
-
 	AST_NOP: 0,
 	AST_ARRAY: 1,
 	AST_REGEXP: 2,
-
+	AST_THIS: 3,
+	AST_GLOBAL: 4,
 	AST_NOT: 5,
 	AST_AND: 6,
 	AST_OR: 7,
 	AST_TERNARY: 8,
-
 	AST_ADD: 9,
 	AST_SUB: 10,
 	AST_MUL: 11,
@@ -21,29 +20,18 @@ module.exports = {
 	AST_GE: 18,
 	AST_EQ: 19,
 	AST_NEQ: 20,
-
-	AST_IF: 26,
-	AST_VAR: 25,
-	// not done
-	AST_FOR: 27,
-	AST_MACRO: 28,
-	AST_RETURN: 29,
-
-
-	AST_THIS: 3,
-	AST_GLOBAL: 4,
-
-
 	AST_REF: 21,
 	AST_METHOD: 22,
 	AST_PROP: 23,
-
 	AST_CALL: 24,
+	AST_VAR: 25,
+	AST_IF: 26,
+	AST_FOR: 27,
+	AST_MACRO: 28,
+	AST_RETURN: 29,
 	AST_NODES: 30,
 	AST_NODELIST: 31,
-
 	RE_GLOBAL: 0x01,
 	RE_MULTILINE: 0x02,
 	RE_IGNORECASE: 0x04
-
 };
