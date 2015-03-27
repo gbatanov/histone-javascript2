6a9ff47f6cdf51242d9ba8f440aa277e28d57cb8
Ruslan <matveev.ruslan@gmail.com>
2015-03-27 11:39:27 +0300

    fix caches

--- src/types/Global.js
+++ src/types/Global.js
@@ -14,6 +14,16 @@ var MONTH_NAMES_SHORT = ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Ию
 var MONTH_NAMES_LONG = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
 
+function getCache(cacheKey, load, process) {
+	if (cacheKey) cacheKey = JSON.stringify(cacheKey);
+	if (!cacheKey || !RESOURCE_CACHE.hasOwnProperty(cacheKey)) {
+		load(function(result) {
+			if (cacheKey) RESOURCE_CACHE[cacheKey] = result;
+			process(result);
+		});
+	} else process(RESOURCE_CACHE[cacheKey]);
+}
+
 RTTI_register(RTTI_T_GLOBAL, 'toString', '(Global)');
 
 RTTI_register(RTTI_T_GLOBAL, 'getBaseURI', function(self, args, scope) {
@@ -94,13 +104,11 @@ RTTI_register(RTTI_T_GLOBAL, 'loadText', function(self, args, scope, ret) {
 	if (typeof requestURI !== 'string') return ret();
 	requestURI = Utils_resolveURI(requestURI, scope.getBaseURI());
 	if (typeof requestURI !== 'string') return ret();
-	var cacheKey = JSON.stringify(['loadText', requestURI]);
-	if (RESOURCE_CACHE.hasOwnProperty(cacheKey))
-		return ret(RESOURCE_CACHE[cacheKey]);
-	RTTI.loadResource(requestURI, function(result) {
-		result = (typeof result === 'string' ? result : undefined);
-		ret(RESOURCE_CACHE[cacheKey] = result);
-	});
+	getCache(['loadText', requestURI], function(ret) {
+		RTTI.loadResource(requestURI, function(result) {
+			ret(typeof result === 'string' ? result : undefined);
+		});
+	}, ret);
 });
 
 RTTI_register(RTTI_T_GLOBAL, 'loadJSON', function(self, args, scope, ret) {
@@ -108,17 +116,16 @@ RTTI_register(RTTI_T_GLOBAL, 'loadJSON', function(self, args, scope, ret) {
 	if (typeof requestURI !== 'string') return ret();
 	requestURI = Utils_resolveURI(requestURI, scope.getBaseURI());
 	if (typeof requestURI !== 'string') return ret();
-	var cacheKey = JSON.stringify(['loadText', requestURI]);
-	if (RESOURCE_CACHE.hasOwnProperty(cacheKey))
-		return ret(RESOURCE_CACHE[cacheKey]);
-	RTTI.loadResource(requestURI, function(result) {
-		if (typeof result === 'string') try {
-			result = result.replace(/^\s*([$A-Z_][0-9A-Z_$]*)?\s*\(\s*/i, '');
-			result = result.replace(/\s*\)\s*(;\s*)*\s*$/, '');
-			ret(RESOURCE_CACHE[cacheKey] = JSON.parse(result));
-		} catch (exception) {}
-		ret(RESOURCE_CACHE[cacheKey] = undefined);
-	});
+	getCache(['loadJSON', requestURI], function(ret) {
+		RTTI.loadResource(requestURI, function(result) {
+			if (typeof result === 'string') try {
+				result = result.replace(/^\s*([$A-Z_][0-9A-Z_$]*)?\s*\(\s*/i, '');
+				result = result.replace(/\s*\)\s*(;\s*)*\s*$/, '');
+				ret(JSON.parse(result));
+			} catch (exception) {}
+			ret();
+		});
+	}, ret);
 });
 
 RTTI_register(RTTI_T_GLOBAL, 'require', function(self, args, scope, ret) {
@@ -126,29 +133,17 @@ RTTI_register(RTTI_T_GLOBAL, 'require', function(self, args, scope, ret) {
 	if (typeof requestURI !== 'string') return ret();
 	requestURI = Utils_resolveURI(requestURI, scope.getBaseURI());
 	if (typeof requestURI !== 'string') return ret();
-
-	var resultCache, thisObj = (args.length > 1 ? args[1] : undefined);
-
-	if (thisObj === undefined) {
-		resultCache = JSON.stringify(['require', requestURI]);
-		if (RESOURCE_CACHE.hasOwnProperty(resultCache)) {
-			return ret(RESOURCE_CACHE[resultCache]);
-		}
-	}
-
-	var template = JSON.stringify(['template', requestURI]);
-	if (!RESOURCE_CACHE.hasOwnProperty(template)) {
-
-		// $tpl = Histone::loadResource($requestURI, array('method' => 'GET'));
-		// if (is_string($tpl)) $tpl = Histone::parse($tpl, $requestURI);
-		// $template = Histone_Global::setCache($template, $tpl);
-
-	} else template = RESOURCE_CACHE[template];
-
-	RTTI.loadResource(requestURI, function(template) {
-		template = Runtime.parseTemplate(template, requestURI);
-		var runtime = new Runtime(requestURI, thisObj)
-		runtime.process(template, ret);
-	});
-
+	getCache(args.length === 1 && ['templateResult', requestURI], function(ret) {
+		getCache(['templateAST', requestURI], function(ret) {
+			RTTI.loadResource(requestURI, function(template) {
+				if (typeof template !== 'string') template = undefined;
+				else template = Runtime.parseTemplate(template, requestURI);
+				ret(template);
+			});
+		}, function(templateAST) {
+			if (typeof templateAST === 'undefined') return ret();
+			var runtime = new Runtime(requestURI, args[1]);
+			runtime.process(templateAST, ret);
+		});
+	}, ret);
 });
