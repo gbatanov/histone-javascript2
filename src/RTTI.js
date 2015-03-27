aa9b6cc009de9215c699dfebf903839373a75b54
Ruslan <matveev.ruslan@gmail.com>
2015-03-27 21:45:23 +0300

    cleanup

--- src/RTTI.js
+++ src/RTTI.js
@@ -1,7 +1,7 @@
-var Utils = require('./Utils.js');
-var Constants = require('./Constants.js');
-var HistoneMacro = require('./Macro.js');
-var HistoneArray = require('./Array.js');
+var Utils = require('./Utils'),
+	HistoneArray = require('./Array'),
+	Constants = require('./Constants');
+
 var isNumeric = Utils.isNumeric;
 var forEachAsync = Utils.forEachAsync;
 
@@ -110,135 +110,7 @@ function getGlobal() {
 	return globalObject;
 }
 
-function newArray(items) {
-	var array = new HistoneArray();
-	for (var c = 0; c < items.length; c++)
-		array.push(items[c][0], items[c][1]);
-	return array;
-}
 
-function newMacro(params, body, scope) {
-	return new HistoneMacro(params, body, scope);
-}
-
-
-function processEquality(left, right) {
-
-	if (typeof left === 'string' && typeof right === 'number') {
-		if (isNumeric(left)) left = parseFloat(left);
-		else right = toString(right);
-	}
-
-	else if (typeof left === 'number' && typeof right === 'string') {
-		if (isNumeric(right)) right = parseFloat(right);
-		else left = toString(left);
-	}
-
-	if (!(typeof left === 'string' && typeof right === 'string')) {
-		if (typeof left === 'number' && typeof right === 'number') {
-			left = parseFloat(left);
-			right = parseFloat(right);
-		} else {
-			left = toBoolean(left);
-			right = toBoolean(right);
-		}
-	}
-
-	return (left === right);
-
-}
-
-function processRelational(type, left, right) {
-
-	if (typeof left === 'string' && typeof right === 'number') {
-		if (isNumeric(left)) left = parseFloat(left);
-		else right = toString(right);
-	}
-
-	else if (typeof left === 'number' && typeof right === 'string') {
-		if (isNumeric(right)) right = parseFloat(right);
-		else left = toString(left);
-	}
-
-	if (!(typeof left === 'number' && typeof right === 'number')) {
-		if (typeof left === 'string' && typeof right === 'string') {
-			left = left.length;
-			right = right.length;
-		} else {
-			left = toBoolean(left);
-			right = toBoolean(right);
-		}
-	}
-
-	switch (type) {
-		case Constants.AST_LT: return (left < right);
-		case Constants.AST_GT: return (left > right);
-		case Constants.AST_LE: return (left <= right);
-		case Constants.AST_GE: return (left >= right);
-	}
-
-}
-
-function processUnaryMinus(value) {
-	value = Utils.toNumber(value);
-	if (typeof value === 'number') return (-value);
-}
-
-
-
-function processArithmetical(type, left, right) {
-
-	if (isNumeric(left)) left = parseFloat(left);
-	if (typeof left !== 'number') return;
-
-	if (isNumeric(right)) right = parseFloat(right);
-	if (typeof right !== 'number') return;
-
-	switch (type) {
-		case Constants.AST_SUB: return (left - right);
-		case Constants.AST_MUL: return (left * right);
-		case Constants.AST_DIV: return (left / right);
-		case Constants.AST_MOD: return (left % right);
-	}
-
-}
-
-function processAddition(left, right) {
-	// console.info(left, '+', right)
-
-
-
-	if (!(typeof left === 'string' || typeof right === 'string')) {
-
-		if (isNumeric(left) || isNumeric(right)) {
-			if (isNumeric(left)) left = parseFloat(left);
-			if (typeof left !== 'number') return;
-			if (isNumeric(right)) right = parseFloat(right);
-			if (typeof right !== 'number') return;
-			return (left + right);
-		}
-
-		if (left instanceof HistoneArray &&
-			right instanceof HistoneArray) {
-			return array_merge($left, $right);
-		}
-	}
-
-	return (toString(left) + toString(right));
-}
-
-function iterate(collection, retn, retf) {
-
-	if (collection instanceof HistoneArray && collection.values.length) {
-		var keys = collection.keys, index = 0, last = collection.values.length - 1;
-
-		forEachAsync(collection.values, function(value, next, index) {
-			retn(value, next, keys[index], index, last);
-		}, retf);
-
-	} else return true;
-
-}
 
 
 function toHistone(value) {
@@ -297,28 +169,16 @@ module.exports = {
 	T_ARRAY: 'HistoneArray',
 	T_GLOBAL: 'HistoneGlobal',
 
-
-	processAddition: processAddition,
-	processEquality: processEquality,
-	processRelational: processRelational,
-	processArithmetical: processArithmetical,
-	processUnaryMinus: processUnaryMinus,
-
-	toString: toString,
-	toBoolean: toBoolean,
-	toJSON: toJSON,
-	iterate: iterate,
+	getGlobal: getGlobal,
+	toHistone: toHistone,
+	register: register,
+	setResourceLoader: setResourceLoader,
+	loadResource: loadResource,
 
 	callSync: callSync,
 	callAsync: callAsync,
 
-	getGlobal: getGlobal,
-	newArray: newArray,
-	newMacro: newMacro,
-
-	toHistone: toHistone,
-	register: register,
-	setResourceLoader: setResourceLoader,
-	loadResource: loadResource
-
+	toString: toString,
+	toBoolean: toBoolean,
+	toJSON: toJSON
 };
